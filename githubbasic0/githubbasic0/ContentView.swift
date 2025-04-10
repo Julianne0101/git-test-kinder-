4a152a3b9103d47dff26c079b2e856b638cefc5e
Julianne0101 <dlwndms2000001@gmail.com>
2025-04-10 20:08:59 +0900

    버튼 추가

---
+++ githubbasic0/githubbasic0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  githubbasic0
+//
+//  Created by 이주은 on 4/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ githubbasic0/githubbasic0/githubbasic0App.swift
@@ -0,0 +1,17 @@
+//
+//  githubbasic0App.swift
+//  githubbasic0
+//
+//  Created by 이주은 on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct githubbasic0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
